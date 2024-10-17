8a41b9f4d28f90c0244830bbf8a7602c89e0c538
dosrhd <dosrhd1@gmail.com>
2024-10-17 10:35:09 +0900

    테이블 변경에 다른 불필요한 파일 삭제

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -1,36 +1,13 @@
 package com.spring.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;  // 이 줄을 추가하세요
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.spring.entity.Contest;
-import com.spring.entity.Shorts;
-import com.spring.service.HomeService;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private HomeService homeService;
-
     @GetMapping("/")
-    public String home(@RequestParam(defaultValue = "0") int page, 
-                       @RequestParam(defaultValue = "5") int size, 
-                       Model model) {
-        Page<Shorts> popularShorts = homeService.getPopularShorts(page, size);
-        List<Contest> topContests = homeService.getTopThreeContests();
-        
-        model.addAttribute("popularShorts", popularShorts.getContent());
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", popularShorts.getTotalPages());
-        model.addAttribute("topContests", topContests);
-        
+    public String home() {
         return "index";
     }
 
---
+++ src/test/java/com/spring/newface/NewfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.newface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
