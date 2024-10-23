2c8dc849e64728f8c2363cc6d919e9bab7440b2b
gptjs0629 <s20194441@gmail.com>
2024-10-23 09:16:19 +0900

    10/23 캐릭터 소개 + 상세보기 추가 - 혜선

---
+++ src/main/java/com/spring/controller/PageController.java
@@ -0,0 +1,29 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class PageController {
+    
+    // 캐릭터 소개 게시판으로 이동
+    @GetMapping("character_intro")
+    public String character_intro() {
+        return "character_intro";
+    }
+
+    // 캐릭서 소개 상세 페이지로 이동
+    @GetMapping("character_intro_detail")
+    public String character_intro_detail() {
+        return "character_intro_detail";
+    }
+    
+
+    // 자랑 게시판
+
+
+    // 고객 지원 
+    
+}
