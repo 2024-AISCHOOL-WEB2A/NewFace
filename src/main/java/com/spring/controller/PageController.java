5c219c743581be165490a8e74c9425272d3d291d
dosrhd <dosrhd1@gmail.com>
2024-10-24 17:44:51 +0900

    연주 추가 파일

--- src/main/java/com/spring/controller/PageController.java
+++ src/main/java/com/spring/controller/PageController.java
@@ -9,27 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
     
-    // 캐릭터 소개 게시판으로 이동
-    @GetMapping("character_intro")
-    public String character_intro() {
-        return "character_intro";
-    }
-
-    // 캐릭서 소개 상세 페이지로 이동
-    @GetMapping("character_intro_detail")
-    public String character_intro_detail() {
-        return "character_intro_detail";
-    }
-    
-
-    // 자랑 게시판
-    @GetMapping("pride_board")
-    public String pride_board() {
-        return "pride_board";
-    }
-    
-
-
     // 고객 지원 
     @GetMapping("support")
     public String support() {
