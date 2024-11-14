de4a04f22d4cc172504782a73e16d2f1a26ed6d6
rlwn3434 <rlwn3434@gmail.com>
2024-11-14 12:32:07 +0900

    이미지 확장자 변경 및 프런트엔드 기능 추가

--- src/main/java/com/spring/controller/StartVirtualController.java
+++ src/main/java/com/spring/controller/StartVirtualController.java
@@ -1,13 +1,34 @@
 package com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.entity.PointPayment;
+import com.spring.entity.User;
+import com.spring.service.PointPaymentService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class StartVirtualController {
+
+    @Autowired
+    private PointPaymentService pointPaymentService;  
+
     // 게임하기 이동
     @GetMapping("/start_virtual")
-    public String startController() {
-        return "redirect:/index.html";
+    public String startController(Model model, HttpSession session) {
+        User loginUser = (User) session.getAttribute("loginUser");
+        if(loginUser != null){
+            int userIdx = loginUser.getUserIdx();
+            PointPayment activeRental = pointPaymentService.getActiveRental(userIdx, 14);
+            if(activeRental != null){
+                model.addAttribute("rentalStatus", activeRental.getCharacterIdx());
+                System.out.println(model);
+            }
+        }
+        return "index";
     }
 }
